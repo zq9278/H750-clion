639b9c82572fe561d67823e6390ffc2025a8999d
zhangqi <zq9278@gmail.com>
2024-03-27 18:42:00 +0800

    lvgl驱动成功，双缓存。触摸还没弄

--- lvgl/lv_port_disp_template.c
+++ lvgl/lv_port_disp_template.c
@@ -216,7 +216,6 @@ static void disp_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_colo
 //            }
 //        }
 //    }
-
     /*IMPORTANT!!!
      *Inform the graphics library that you are ready with the flushing*/
     lv_disp_flush_ready(disp_drv);
